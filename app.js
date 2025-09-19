f6a22434706a99e400b249034c24923c0df7a5a7
Emils <emils@gmail.com>
2025-09-19 13:49:17 +0300

    Made so fetch works, and for testing it shows the information in console

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
 const title = document.getElementById("title");
 const body = document.getElementById("body");
 
-
+async function generatePost(){
+    try {
+        const res = await fetch (' https://jsonplaceholder.typicode.com/posts');
+        const json = await res.json();
+        console.log(json);
+    } catch (error) {
+        console.log("nav labi")
+    }
+}
