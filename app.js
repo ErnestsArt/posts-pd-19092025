c0e1add70ae7af37be654afe16660b8ed32c4be8
Emils <emils@gmail.com>
2025-09-19 14:28:42 +0300

    pievienots showPost funckija, kas neizvada infromaciju uz ekaraana

--- app.js
+++ app.js
@@ -5,8 +5,22 @@ async function generatePost(){
     try {
         const res = await fetch (' https://jsonplaceholder.typicode.com/posts');
         const json = await res.json();
-        console.log(json);
+        // console.log(json)
+        const newArr = json.map(showPosts);
+
+        
+        
+
     } catch (error) {
         console.log("nav labi")
+    }   
+
     }
-}
+
+    function showPosts(newArr) {
+    
+    document.getElementById("posts").innerHTML = newArr.join("");
+    }
+
+
+
